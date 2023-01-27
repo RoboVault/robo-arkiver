3fb032aeef48d0c5dff53f7f5e27d1ef76b76f03
SmoothBot <smoothbot123@gmail.com>
2023-01-27 23:02:44 +1100

    Read Arkives Edge Function working

---
+++ supabase/functions/arkives/index.ts
@@ -0,0 +1,63 @@
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-type,Accept,X-Custom-Header,Authorization',
+}
+
+serve(async (req) => {
+
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  try {
+    const supabaseUser = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseUser.auth.getUser()
+
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabase = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
+    )
+
+    const data = req.body ? (await req.json()) : {}
+    const alerts = data.id ? 
+      await supabase
+        .from('arkive')
+        .select('*')
+        .eq('id', data.id)
+        .eq('user_id', user.id) : 
+      await supabase
+        .from('arkive')
+        .select('*')
+        .eq('user_id', user.id)
+
+    return new Response(JSON.stringify({ data: alerts }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+    } catch (error) {
+      console.log(error)
+      return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  } 
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
