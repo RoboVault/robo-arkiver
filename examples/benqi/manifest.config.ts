87bddd908af30d34a5efc4f7ae3f457de09d8011
hazelnutcloud <ha5anmukhli50@gmail.com>
2023-02-15 12:04:14 +0800

    added rest of tokens

--- examples/benqi/manifest.config.ts
+++ examples/benqi/manifest.config.ts
@@ -24,8 +24,44 @@ export const manifest: IManifest = {
               startBlockHeight: 16578216,
             },
             {
-              address: "0xB715808a78F6041E46d61Cb123C9B4A27056AE9C", //qiUSDC
+              address: "0xB715808a78F6041E46d61Cb123C9B4A27056AE9C", //qiUSDCn
               startBlockHeight: 13319519,
+            },
+            {
+              address: "0xe194c4c5aC32a3C9ffDb358d9Bfd523a0B6d1568", // qiBTC
+              startBlockHeight: 3046690,
+            },
+            {
+              address: "0x334AD834Cd4481BB02d09615E7c11a00579A7909", // qiETH
+              startBlockHeight: 3046701,
+            },
+            {
+              address: "0x4e9f683A27a6BdAD3FC2764003759277e93696e6", // qiLINK
+              startBlockHeight: 3046723,
+            },
+            {
+              address: "0xc9e5999b8e75C3fEB117F6f73E664b9f3C8ca65C", // qiUSDt
+              startBlockHeight: 3046718,
+            },
+            {
+              address: "0xBEb5d47A3f720Ec0a390d04b4d41ED7d9688bC7F", // qiUSDC
+              startBlockHeight: 3620405,
+            },
+            {
+              address: "0xd8fcDa6ec4Bdc547C0827B8804e89aCd817d56EF", // qiUSDTn
+              startBlockHeight: 13319600,
+            },
+            {
+              address: "0x835866d37AFB8CB8F8334dCCdaf66cf01832Ff5D", // qiDAI
+              startBlockHeight: 3046729,
+            },
+            {
+              address: "0x872670CcAe8C19557cC9443Eff587D7086b8043A", // qiBUSD
+              startBlockHeight: 21221137,
+            },
+            {
+              address: "0x35Bd6aedA81a7E5FC7A7832490e71F757b0cD9Ce", // qiQI
+              startBlockHeight: 4408541,
             },
           ],
           eventQueries: [
@@ -106,6 +142,22 @@ export const manifest: IManifest = {
             {
               address: "0x9450A29eF091B625e976cE66f2A5818e20791999", // AVAX/USD
               startBlockHeight: 3046672,
+            },
+            {
+              address: "0xEfaa69f461E0aaf0be1798b01371Daf14AC55eA8", // ETH/USD
+              startBlockHeight: 3046701,
+            },
+            {
+              address: "0xA2e5d3254F7d6E8C051Afb7F2aeea0dABf21F750", // LINK/USD
+              startBlockHeight: 3046723,
+            },
+            {
+              address: "0xB6f7e0129439829a3679BD06102fDCAA41ebeE5e", // QI/USD (phase 1)
+              startBlockHeight: 4408541,
+            },
+            {
+              address: "0x4bc3BeBb7eB60155f8b38771D9926d9A23dad5B5", // QI/USD (phase 2)
+              startBlockHeight: 15036335,
             },
           ],
         },
