126582c11ed0c248ef970d4c01cfaaf9a62d4537
SmoothBot <smoothbot123@gmail.com>
2023-06-19 00:59:46 +1000

    Supporting relations with graphql resolvers

--- src/graphql/builder.ts
+++ src/graphql/builder.ts
@@ -6,8 +6,41 @@ export const buildSchemaFromEntities = (
 ) => {
 	schemaComposer.clear()
 	for (const { model, list } of entities) {
-		// deno-lint-ignore no-explicit-any
-		const ModelTC = composeMongoose<any>(model)
+
+		const getTC	= (schemaComposer: any, model: any) => {
+			try {
+				return schemaComposer.getAnyTC(model.modelName)
+			} catch(e) {
+				return composeMongoose<any>(model)
+			}
+		}
+
+		const ModelTC = getTC(schemaComposer, model)
+		const addRelation = (path: string, ref: string) => {
+			const refModel = entities.find(e => e.model.modelName === ref)
+			if (refModel) {
+				const RefTC = getTC(schemaComposer, refModel.model)
+				ModelTC.addRelation(path, {
+					resolver: () => RefTC.mongooseResolvers.findMany({ lean: true }),
+					prepareArgs: {
+						_ids: (source: any) => source[path],
+					},
+				})
+			}
+		}
+	
+		model.schema.eachPath((path: string, type: any) => {
+			if (path === '_id')
+				return
+
+			if (type.instance === 'Array' && type.caster.instance === 'ObjectId') {
+				addRelation(path, type.caster.options.ref)
+			}
+
+			if (type.instance === 'ObjectId') {
+				addRelation(path, type.options.ref)
+			}
+		})
 
 		schemaComposer.Query.addFields({
 			[model.modelName]: ModelTC.mongooseResolvers.findOne({ lean: true }),
