9eeca6ee8e09d445c0ae7415c150827d88ba13cd
hazelnutcloud <hsnmkls@proton.me>
2023-07-04 23:05:37 +0800

    fix example/simple

--- examples/simple/deps.ts
+++ examples/simple/deps.ts
@@ -1,2 +1,6 @@
 export { formatUnits } from 'npm:viem'
-export { createEntity, type EventHandlerFor, Manifest } from '../../mod.ts'
+export {
+  createEntity,
+  type EventHandlerFor,
+  Manifest,
+} from 'https://deno.land/x/robo_arkiver@v0.4.15/mod.ts'
